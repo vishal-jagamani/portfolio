45e9645297a5606add3c0522abbeb219604b614d
Vishal Jagamani <vishaljagamani20@gmail.com>
2024-10-15 11:40:44 +0530

    eslint error comment

--- src/components/aceternityui/DotBackground.tsx
+++ src/components/aceternityui/DotBackground.tsx
@@ -4,9 +4,10 @@ interface props {
     className: string | null;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const DotBackground: React.FC<props> = ({ className }) => {
     return (
-        <div className="dark:bg-dot-white/[0.4] bg-dot-black/[0.4] fixed flex h-screen w-full items-center justify-center bg-white dark:bg-black">
+        <div className="fixed flex h-screen w-full items-center justify-center bg-white bg-dot-black/[0.4] dark:bg-black dark:bg-dot-white/[0.4]">
             {/* Radial gradient for the container to give a faded look */}
             <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
         </div>
